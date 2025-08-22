656e845840a4a24e34eca80f309c70baba4a49d7
TheRomeroGuilherme <guilherme.g4m3s@gmail.com>
2025-08-22 19:22:48 -0300

    deixando do formato conforme o notion

--- arduino-battery-health-status-API/main.ino
+++ arduino-battery-health-status-API/main.ino
@@ -1,9 +1,31 @@
+//=== Importacoes e bibliotecas ===//
+#include <Arduino.h>
+
+
+//=== Instâncias, variáveis globais e funções ===//
 const int Pino_Tensao = A1;
 const int Pino_Corrente = A0;
 
 int Leitura_Bruta_Tensao;
 int Leitura_Bruta_Corrente;
 
+void coletar_e_enviar_dados() {
+  int Leitura_Bruta_Tensao;
+  int Leitura_Bruta_Corrente;
+
+  Leitura_Bruta_Tensao = analogRead(Pino_Tensao);
+  Leitura_Bruta_Corrente = analogRead(Pino_Corrente);
+
+  Serial.print("tensao: ");
+  Serial.println(Leitura_Bruta_Tensao);
+
+  Serial.print("corrente: ");
+  Serial.println(Leitura_Bruta_Corrente);
+
+  Serial.println("---");
+}
+
+//=== Função principais setup() e loop() ===//
 void setup() {
   
   Serial.begin(115200);
@@ -13,14 +35,7 @@ void setup() {
 }
 
 void loop() {
-
-  Leitura_Bruta_Tensao = analogRead(Pino_Tensao);
-  Leitura_Bruta_Corrente = analogRead(Pino_Corrente);
-  Serial.print("tensao: ");
-  Serial.println(Leitura_Bruta_Tensao);
-  Serial.print("corrente: ");
-  Serial.println(Leitura_Bruta_Corrente);
-  Serial.println("---");
+  coletar_e_enviar_dados();
   
   delay(2000);
 }
