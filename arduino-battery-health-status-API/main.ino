8e16f92b84ade7bb44212b3f820450a363000ef5
TheRomeroGuilherme <guilherme.g4m3s@gmail.com>
2025-08-22 19:14:21 -0300

    código arduíno finalizado

--- arduino-battery-health-status-API/main.ino
+++ arduino-battery-health-status-API/main.ino
@@ -1,30 +1,26 @@
-// Inclui os headers dos nossos módulos
-#include "config.h"
-#include "wifi-manager/wifi_manager.h"
-#include "api-client/api_client.h"
+const int Pino_Tensao = A1;
+const int Pino_Corrente = A0;
 
-// A função setup() é executada uma vez quando o dispositivo liga
+int Leitura_Bruta_Tensao;
+int Leitura_Bruta_Corrente;
+
 void setup() {
+  
   Serial.begin(115200);
-  delay(1000); // Uma pequena pausa para o monitor serial iniciar
+  delay(100);
 
-  setupWiFi(); // Chama a função do nosso módulo de Wi-Fi
+  Serial.println("Iniciando a coleta de dados de tensao e corrente...");
 }
 
-// A função loop() é executada repetidamente
 void loop() {
-  // 1. Simula a leitura dos sensores (aqui você colocaria o código real dos seus sensores)
-  float voltage = 3.8 + random(-3, 3) / 10.0;
-  float current = (voltage > 4.0) ? 0.8 : -0.5; // Simula carga/descarga
-  float temperature = 38.0 + random(-5, 5);
 
-  Serial.println("\n--- Nova Leitura ---");
-  Serial.println("Voltagem Lida: " + String(voltage) + "V");
-
-  // 2. Chama a função do nosso módulo de API para enviar os dados
-  sendBatteryData(voltage, current, temperature);
-
-  // 3. Aguarda o intervalo definido no config.h antes da próxima leitura
-  Serial.println("Aguardando próximo ciclo...");
-  delay(UPDATE_INTERVAL_MS);
+  Leitura_Bruta_Tensao = analogRead(Pino_Tensao);
+  Leitura_Bruta_Corrente = analogRead(Pino_Corrente);
+  Serial.print("tensao: ");
+  Serial.println(Leitura_Bruta_Tensao);
+  Serial.print("corrente: ");
+  Serial.println(Leitura_Bruta_Corrente);
+  Serial.println("---");
+  
+  delay(2000);
 }
--- arduino-battery-health-status-API/src/api-client/api_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef API_CLIENT_H
-#define API_CLIENT_H
-
-// Declara a função que envia os dados para a API
-// Ela recebe os valores dos sensores como parâmetros
-void sendBatteryData(float voltage, float current, float temperature);
-
-#endif
